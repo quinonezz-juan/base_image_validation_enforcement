3958b86e8b1fc87a0a7636acce3ae90e8897200e
quinonezz-juan <94247098+quinonezz-juan@users.noreply.github.com>
2023-06-26 08:40:10 -0500

    Add files via upload

---
+++ taginherit.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Catch the required parameters
+while getopts 'B:T:R:' OPTION; do
+ case "$OPTION" in
+  B)
+   baseimageName="$OPTARG"
+   echo $baseimageName
+   ;;
+
+  T)
+   baseimageTag="$OPTARG"
+   echo $baseimageTag
+   ;;
+
+  R)
+   output_image_name="$OPTARG"
+   echo $output_image_name
+   ;;
+
+  ?)
+   echo "ERROR: Wrong Execution"
+   echo "script usage: taginherit.sh [-B Base Image Name] [-T Base Image Tag] [-R Result Image Name]" >&2
+   exit 1
+   ;;
+
+ esac
+done
+
+# Determine if the base image used in the project corresponds to one of the Standarized
+
+valid_images_id=('base_image')
+
+for i in ${valid_images_id[@]}
+do
+    if [ $i = $baseimageName ]; then
+	# Determine the tags of the base image
+	baseimageID="$baseimageName"':'"$baseimageTag"
+	echo $baseimageID
+	base_image_tags=$(docker image inspect --format='{{join .RepoTags "\n"}}' $baseimageID)
+	base_image_name=$(echo ${base_image_tags[0]} | cut -d ':' -f1)
+	echo $base_image_name
+	# Build the new image
+	docker build --build-arg IMAGE_NAME=$base_image_name --build-arg IMAGE_TAG=$baseimageTag -t $output_image_name -f Dockerfile .
+
+	# Assing taf to the image
+	for tag in $base_image_tags
+	do
+ 	 separate_tag=(${tag//:/ })
+ 	 #echo ${separate_tag[1]}
+ 	 docker tag $output_image_name:latest $output_image_name:${separate_tag[1]}
+	done
+	break
+    fi
+done
